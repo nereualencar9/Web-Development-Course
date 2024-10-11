ee7a31c3ddf8f2e8408ff4ebd36c4d2d6f35af73
nereualencar9 <nereualencar9@gmail.com>
2024-10-11 20:31:58 -0300

    add class 01 data types

---
+++ JavaScript/class-01-data-types/script.js
@@ -0,0 +1,24 @@
+let myName = "Nereu Alencar"; // string
+let age = 43; // number
+const cpf = "123"; // string
+let isAdmin = true; // boolean
+
+myName = "nereu"
+
+
+console.log(myName);
+console.log(cpf)
+console.log(isAdmin)
+
+console.log(typeof myName)
+console.log(typeof cpf)
+console.log(typeof isAdmin)
+
+document.write("Bem vindo, " + myName);
+console.log(myName + 1) // type cohesion
+console.log(Number("2") + 3); // Type coversion 
+console.log(typeof Number("2"));
+console.log(typeof String(age));
+
+// template literals (string)
+console.log(`Meu nome é ${myName} e tenho ${age} anos`);
