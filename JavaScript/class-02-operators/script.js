949e4bec66c1c9f0773d631ab2ae2ab852f6cc36
nereualencar9 <nereualencar9@gmail.com>
2024-10-15 20:23:05 -0300

    add class-02- operators

---
+++ JavaScript/class-02-operators/script.js
@@ -0,0 +1,55 @@
+let num1 = 2;
+let num2 = 3;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 ** num2);
+console.log(81 ** (1 / 2));
+
+// assigment
+num1 = 10;
+num2 = num2 + 2;
+num1 = num1 - 6;
+
+num1 += 4
+num2 -= 2
+
+console.log(num1);
+console.log(num2);
+
+//comparison
+console.log(10 == 10);
+console.log(10 != 10);
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 2);
+console.log(2 <= 2);
+
+console.log(20 == "20")
+console.log(20 === "20")
+
+// logical (&& ||)
+const card = false;
+const credit = true;
+const cash = true
+
+// disney
+console.log(card && credit); // True True
+console.log(credit || cash); // False Flase
+
+// truth table
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+
+
+
+
