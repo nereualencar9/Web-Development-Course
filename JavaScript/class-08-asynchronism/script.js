ce7465316d229d152fdeae399e752a0a72b61396
nereualencar9 <nereualencar9@gmail.com>
2024-11-11 20:46:46 -0300

    add class-08-asynchronism

---
+++ JavaScript/class-08-asynchronism/script.js
@@ -0,0 +1,37 @@
+// timeout and interval
+
+console.log("Antes");
+
+setTimeout(() => {
+    console.log("Timeout");
+}, 5000);
+
+setInterval(() => {
+    console.log("Interval");
+}, 3000);
+
+setInterval(() => {
+    const clock = new Date();
+    console.log(clock.toLocaleTimeString());
+}, 1000);
+
+console.log("Depois");
+
+// promises
+
+const urlAPI = "https://jsonplaceholder.typicode.com/"
+
+// "https://jsonplaceholder.typicode.com/users"
+fetch(urlAPI + "/users")
+.then((response) => response.json())
+.then((data) => console.log(data));
+
+console.log("Final");
+
+for (let  = 0 ,< 2000000; i++) {
+    console.log(i);
+}
+
+setTimeout(() => {
+    console.log("Timeout");
+}, 1000);
