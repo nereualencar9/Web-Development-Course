ac62df430b51975cc59c3e3741dbdd1ab3812c99
nereualencar9 <nereualencar9@gmail.com>
2025-01-10 20:21:21 -0300

    add use effect example

--- 3-React/class-03-hooks/src/App.tsx
+++ 3-React/class-03-hooks/src/App.tsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
   const [users, setUsers] = useState(["user1", "user2"]);
+
   console.log(count);
 
   function add() {
     setCount(count + 1);
   }
 
+  function handleAddUser() {
+    const number = users.length + 1;
+    setUsers([...users, "user" + number]);
+  }
+  function handleRemoveUser(name: string) {
+    const filterArray = users.filter((user) => user != name);
+    setUsers(filterArray);
+  }
   function handleName(event: React.ChangeEvent<HTMLInputElement>) {
     setName(event.target.value);
   }
+
+  // always run when the component is rendered
+  useEffect(() => {
+    console.log("Renderizou!");
+  }, [count, name]); // dependency array
+
+  console.log("AAAA");
 
   return (
     <>
@@ -24,8 +40,15 @@ export function App() {
       <p>Name: {name}</p>
       <input type="text" onInput={handleName} />
 
+      <h2>User</h2>
+      <button onClick={handleAddUser}>add+</button>
+
       {users.map((user) => {
-        return <p key={user}>{user}</p>;
+        return (
+          <p key={user}>
+            {user} <button onClick={() => handleRemoveUser(user)}>X</button>
+          </p>
+        );
       })}
     </>
   );
