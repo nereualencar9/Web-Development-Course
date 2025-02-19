ea8c252f7788cb85cf5bd006ed943ff5ec861f56
nereualencar9 <nereualencar9@gmail.com>
2025-02-19 20:39:27 -0300

    update class-02-routes

--- Node.js/class-02-routes/src/routes/index.ts
+++ Node.js/class-02-routes/src/routes/index.ts
@@ -1,7 +1,40 @@
 import { response, Router } from "express";
 
-export const router =Router();
+export const router = Router();
 
 router.get("/", (_request, response) => {
-    response.json({ name: "Class Routes" });
+  response.json({ name: "Class Routes" });
+});
+
+router.get("/user", (_req, res) => {
+  res.json([
+    { name: "Ash", age: 10 },
+    { name: "Mestre Came", Age: 300 },
+    { name: "Alucard", Age: 500 },
+  ]);
+});
+
+// route params
+router.get("/user/:id", (req, res) => {
+  const { id } = req.params;
+
+  const users = [
+    { id: 1, name: "Ash", age: 10 },
+    { id: 2, name: "Mestre Came", Age: 300 },
+    { id: 3, name: "Alucard", Age: 500 },
+  ];
+
+  res.json(users[Number(id) - 1] || {});
+});
+
+// query params
+router.get("/user", (req, res) => {
+  const { id, name, value } = req.query;
+  res.json({ id, name, value });
+});
+
+// body params
+router.post("/user", (req, response) => {
+  const { id, name, value } = req.body;
+  response.json({ id, name, value });
 });
--- Node.js/class-02-routes/src/server.ts
+++ Node.js/class-02-routes/src/server.ts
@@ -5,9 +5,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3333;
 
+// middleware
+app.use(express.json());
 app.use(router);
 
 app.listen(3000, () => {
-  console.log("Server is running on PORT ${PORT}...");
+  console.log(`Server is running on PORT ${PORT}...`);
 });
 
