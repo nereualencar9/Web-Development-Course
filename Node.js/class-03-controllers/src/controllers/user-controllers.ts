c8d747964d1941b4c9467532109e7660331b9858
nereualencar9 <nereualencar9@gmail.com>
2025-02-24 20:51:08 -0300

    add validation examples

--- Node.js/class-03-controllers/src/controllers/user-controllers.ts
+++ Node.js/class-03-controllers/src/controllers/user-controllers.ts
@@ -1,17 +1,35 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/app-error";
+import { z } from "zod";
+
+// zod
+const UserSchema = z.object({
+  name: z.string({message: "Nome é obrigatório"}).min(3, "Minimo de 3 caracteres!"),
+  series:z.string(). max(255, "Minimo de 255 caracteres!"),
+  age: z.number(),
+  email: z.string().email(),
+});
 
 export const userControllers = {
-  create(req: Request, res: Response) {
-    const { name, series } = req.body;
+  create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { name, series } =  UserSchema.parse(req.body);
 
-    console.log({ name, series });
+      if (!name || !series) {
+        throw new AppError(500, "Todos os dados são obrigatórios");
+      }
 
-    return res.status(201).json({ message: "user created" });
+      console.log({ name, series });
+
+      res.status(201).json({ message: "user created" });
+    } catch (error) {
+      next(error);
+    }
   },
   read(req: Request, res: Response) {
     const { id } = req.params;
 
-    return res.status(200).json({ message: `user ${id} read` });
+    res.status(200).json({ message: `user ${id} read` });
   },
 
   update(req: Request, res: Response) {
@@ -20,11 +38,11 @@ export const userControllers = {
 
     console.log({ id, name, series });
 
-    return res.status(200).json({ message: "user update" });
+    res.status(200).json({ message: "user update" });
   },
 
   delete(req: Request, res: Response) {
     const { id } = req.params;
-    return res.status(200).json({ message: `user ${id} deleted` });
+    res.status(200).json({ message: `user ${id} deleted` });
   },
 };
--- Node.js/class-03-controllers/src/server.ts
+++ Node.js/class-03-controllers/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { router } from "./routes";
+import { appErrors } from "./errors/app-errors";
 
 const app = express();
 
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 3333;
 // middleware
 app.use(express.json());
 app.use(router);
+
+app.use(appErrors);
 
 app.listen(3000, () => {
   console.log(`Server is running on PORT ${PORT}...`);
