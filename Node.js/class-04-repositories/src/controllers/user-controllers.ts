38a29b1af38d376f0afce4d36c121cd121afaf6b
nereualencar9 <nereualencar9@gmail.com>
2025-02-27 21:13:44 -0300

    add password hash

--- Node.js/class-04-repositories/src/controllers/user-controllers.ts
+++ Node.js/class-04-repositories/src/controllers/user-controllers.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import { AppError } from "../errors/app-error";
 import { z } from "zod";
 import { sqliteConnction } from "../databases";
+import { randomUUID } from "node:crypto";
+import { compare, hash } from "bcrypt";
 
 // zod
 const UserSchema = z.object({
@@ -17,30 +19,51 @@ export const userControllers = {
     try {
       const { name, email, password } = UserSchema.parse(req.body);
 
+      const passworddHash = await hash(password, 10);
+
       const user = {
-        id: "1",
+        id: randomUUID(),
         name,
         email,
-        password,
+        password: passworddHash,
       };
 
       const db = await sqliteConnction();
 
-      const sqlQuery = "INSERT INTO users(id, name, email, password) VALUES (?, ?, ?, ?)";
+      const sqlQuery =
+        "INSERT INTO users(id, name, email, password) VALUES (?, ?, ?, ?)";
 
-      await db.run(sqlQuery,[user.id, user.name, user.email, user.password]);
+      await db.run(sqlQuery, [user.id, user.name, user.email, user.password]);
 
-      console.log(user);
-
-      res.status(201).json({ message: "user created" });
+      res.status(201).json({ message: "user created", userID: user.id });
     } catch (error) {
       next(error);
     }
   },
-  read(req: Request, res: Response) {
-    const { id } = req.params;
 
-    res.status(200).json({ message: `user ${id} read` });
+  async read(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const { password } = req.body;
+
+      if (!password) {
+        throw new AppError(400, "Password is required!");
+      }
+
+      const db = await sqliteConnction();
+      const querySQL = "SELECT * FROM users WHERE id = ?";
+      const user = await db.get(querySQL, [id]);
+
+      const passwordMatch = await compare(password, user.password);
+
+      if (!passwordMatch) {
+        throw new AppError(401, "Password does not match");
+      }
+
+      res.status(200).json({ message: `user ${id} read`, user });
+    } catch (error) {
+      next(error);
+    }
   },
 
   update(req: Request, res: Response) {
